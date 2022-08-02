b2c415d969957f84a59ef1ebfd115b61aa124d70
ayaneey <ayan.h18@hotmail.com>
2022-08-02 12:05:41 +0100

    implemented timer function set so that it deducts 3 seconds for every incorrect response

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ var containerQuestionEl = document.getElementById("questions_container");
 var containerWelcomeEl = document.getElementById("welcome_page");
 var containerEndEl = document.getElementById("end-container");
 var containerScoreEl = document.getElementById("score-complete");
-var formInitials = document.getElementById("initials-form");
+var formInitialsEl = document.getElementById("initials-form");
 var containerHighScoresEl = document.getElementById("highscore");
-var containerCheckScoreEl = document.getElementById("score_check");
+var containerScoreBoardEl = document.getElementById("score_board");
 var correctEl = document.getElementById("correct");
 var incorrectEl = document.getElementById("incorrect");
+let next_question_button = document.getElementById("next-question-button");
+let containerQuestions = document.getElementById("container-questions");
+let finalScoreEl = document.getElementById("final_output");
 
 // // question, answer and timer elements
 var timerEl = document.querySelector("#timer");
 var questionEl = document.getElementById("question");
 var optionEl = document.getElementById("options");
 // var answerEl = document.getElementById("answer");
-var timeleft;
+var timeleft = 60;
 var score = 0;
 var gameover;
 timerEl.innerText = 0;
@@ -92,42 +95,92 @@ var questions = [
     ],
   },
 ];
-let questionNumber = 2;
+let questionNumber = 1;
 function replaceQuestionDiv() {
   containerWelcomeEl.setAttribute("class", "hide");
   containerQuestionEl.setAttribute("class", "show");
 }
 
+let interval = () => {
+  let timeInterval = setInterval(() => {
+    timeleft--;
+    timerEl.innerHTML = timeleft;
+    if (timeleft == 0) {
+      console.log("if");
+      clearInterval(timeInterval);
+      finalScore();
+    }
+  }, 1000);
+};
+
 // function to populate quiz
 function populateQuizDetails() {
-  let currentQuestion = questions[questionNumber - 1];
-  questionEl.innerHTML = currentQuestion.question;
-  let choices = currentQuestion.choices;
-  let unorderedList = document.createElement("ul");
-  for (let i = 0; i < choices.length; i++) {
-    let listItem = document.createElement("li");
-    listItem.setAttribute("data-answer", currentQuestion.answer);
-    listItem.addEventListener("click", function (event) {
-      console.log(event.target);
+  let currentNumber = questionNumber;
+  if (currentNumber == 6) {
+    timeleft = 1;
+    correctEl.setAttribute("class", "hide");
+    incorrectEl.setAttribute("class", "hide");
+    next_question_button.setAttribute("class", "hide");
+    finalScore();
+  } else {
+    console.log(`value before ${currentNumber}`);
+    next_question_button.setAttribute("class", "hide");
+    correctEl.setAttribute("class", "hide");
+    incorrectEl.setAttribute("class", "hide");
+    optionEl.innerHTML = "";
+    let currentQuestion = questions[currentNumber - 1];
+    questionEl.innerHTML = currentQuestion.question;
+    let choices = currentQuestion.choices;
+    console.log(currentQuestion);
+    let unorderedList = document.createElement("ul");
 
-      let correctAnswer = event.target.dataset.answer;
-      let text = event.target.innerHTML;
-      if (text === correctAnswer) {
-        correctEl.setAttribute("class", "show");
-        incorrectEl.setAttribute("class", "hide");
-      } else {
-        incorrectEl.setAttribute("class", "show");
-        correctEl.setAttribute("class", "hide");
-      }
-    });
-    listItem.innerHTML = choices[i].choice;
-    unorderedList.appendChild(listItem);
+    for (let i = 0; i < choices.length; i++) {
+      let listItem = document.createElement("li");
+      listItem.setAttribute("data-answer", currentQuestion.answer);
+      listItem.addEventListener("click", function (event) {
+        console.log(event.target);
+
+        let correctAnswer = event.target.dataset.answer;
+        let text = event.target.innerHTML;
+        if (text === correctAnswer) {
+          correctEl.setAttribute("class", "show");
+          incorrectEl.setAttribute("class", "hide");
+          next_question_button.setAttribute("class", "show");
+          score += 5;
+        } else {
+          incorrectEl.setAttribute("class", "show");
+          correctEl.setAttribute("class", "hide");
+          next_question_button.setAttribute("class", "show");
+          timeleft -= 3;
+          score -= 2;
+        }
+      });
+      listItem.innerHTML = choices[i].choice;
+      unorderedList.appendChild(listItem);
+    }
+    questionNumber++;
+
+    console.log(questionNumber);
+    optionEl.appendChild(unorderedList);
   }
-  optionEl.appendChild(unorderedList);
+}
+
+function finalScore() {
+  containerQuestions.setAttribute("class", "hide");
+  containerScoreBoardEl.innerHTML = score;
+  containerHighScoresEl.setAttribute("class", "show");
+}
+
+function countDown() {
+  interval();
 }
 
 function initializeQuiz() {
   replaceQuestionDiv();
+  next_question_button.addEventListener("click", function (event) {
+    populateQuizDetails();
+  });
+  countDown();
   populateQuizDetails();
 }
 
